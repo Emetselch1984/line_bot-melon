4a9d4439f9efdb777caa49b6163af05e49d262fb
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-09-12 01:02:43 +0900

    [create]heroku

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module LineBotMelon
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-  
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
