82547caa4b749442d371927a44cc27de9344878c
Emetselch1984 <banzai.nippon2030@gmail.com>
2020-09-12 02:49:24 +0900

    [add]id

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -35,6 +35,7 @@ class LinebotController < ApplicationController
   private
   def client
     @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
